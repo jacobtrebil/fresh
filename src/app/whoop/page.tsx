a20805992f5b36ae997eaab64445b5def0f95b51
Jacob Trebil <jacobtrebil@yahoo.com>
2024-05-02 11:41:26 -0500

    pushhhhhhh baby

--- src/app/whoop/page.tsx
+++ src/app/whoop/page.tsx
@@ -5,13 +5,41 @@ import React, { useState, useEffect } from 'react';
 export default function WhoopData() {
 
   return (
-    <div className="main">
-      <h1>Whoop Metrics</h1>
-      <p>Sleep performance, sleep consistency, sleep efficiency</p>
-      <p>Recovery, resting heart rate, hrv, skin temp</p>
-      <p>Workout performance metrics such as strain and max heart rate</p><br></br>
-      <p>Vibe learns how food impacts your health and helps you make better plans and decisions over time</p><br></br>
-      <p>Vibe builds you diet, workout, and sleep protocols and improves them over time</p>
+    <div className="whoopMain">
+      <div className="whoopMainTop">
+        <h2>Tracks Health Impact of Food</h2>
+        <p>Tracks how food impacts your HRV, RHR, sleep metrics, and additional metrics via Whoop, 8 sleep, or glucose monitors if connected</p>
+      </div>
+      <div className="insights">
+        <h2>Insights</h2><br></br>
+        <ul>
+            <div className="red">
+                <p>High Impact</p>
+            </div>
+            <li>Sleep quality is decreased on days you drank coffee</li><br></br>
+            <div className="yellow">
+                <p>Medium Impact</p>
+            </div>
+            <li>Eating rice increases resting heart rate and lowers hrv</li><br></br>
+            <div className="green">
+                <p>Low Impact</p>
+            </div>
+            <li>Not eating for 4 hours before bed resulted in higher quality sleep</li><br></br>
+        </ul>
+      </div>
+      <div className="protocols">
+        <h2>Protocols</h2>
+        <p>Vibe builds you diet, workout, and sleep protocols and improves them over time</p><br></br>
+        <ul>
+            <li>Nutrition Protocol</li><br></br>
+            <li>Fitness Protocol</li><br></br>
+            <li>Sleep Protocol</li><br></br>
+        </ul>
+      </div>
     </div>
   );
 }
+
+/*       <p>Sleep performance, sleep consistency, sleep efficiency</p>
+      <p>Recovery, resting heart rate, hrv, skin temp</p>
+      <p>Workout performance metrics such as strain and max heart rate</p><br></br> */
