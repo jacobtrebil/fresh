3201a34d06a438ed07af4718beaf31bb150eea1a
Jacob Trebil <jacobtrebil@yahoo.com>
2024-07-31 19:57:07 -0400

    new

--- src/app/fooddetection/page.tsx
+++ src/app/fooddetection/page.tsx
@@ -9,6 +9,8 @@ export default function FoodDetection() {
   // const {  input, handleInputChange, handleSubmit, completion, complete } = useCompletion({ api: '/api/chat' });
 
   const [image, setImage] = useState("");
+  const [images, setImages] = useState([]);
+  const [imageCounter, setImageCounter] = useState(0);
   const [openAIResponse, setOpenAIResponse] = useState({
     food: "",
     calories: "",
@@ -61,9 +63,16 @@ export default function FoodDetection() {
             if (typeof reader.result === 'string') {
                 console.log("reader.result = ", reader.result);
                 setImage(reader.result);
+                setImageCounter(imageCounter + 1);
             }
         }
     }
+
+    useEffect(() => {
+        console.log("imageCounter = ", imageCounter);
+    }, [imageCounter])
+
+    // Could just add a counter and append it to the end + add to the counter each time
 
   const PROMPT_TEMPLATE = 'You take an image and return a list of food items in the image.'
 
@@ -79,33 +88,58 @@ export default function FoodDetection() {
 
   return (
     <div className="main">
-      <h1>Food Detection</h1>
-      { image !== "" ?
-        <img style={{ textAlign: "center", margin: "0 auto" }} src={image} alt="food" width="200" height="200"/>
-            :
-        <p></p> 
-      }
-      <input 
-        type="file"
-        className= "text-sm border rounded -lg cursor-pointer"
-        // accept="image/*"
-        onChange={e => handleFileChange(e)}
-        // onChange={(e) => setImage(e.target.files[0])}
-        /><br></br>
-      <button type="submit" className="button" onClick={handleSubmit}>Submit</button>
-      { openAIResponse?.food !== "" ? 
-      <div>
-        <h2>Food Tracker</h2>
-        <p>Food: {openAIResponse?.food}</p>
-        <p>Calories: {openAIResponse?.calories}</p>
-        <p>Protein: {openAIResponse?.protein}</p>
-        <p>{new Date().toLocaleString()}</p>
-    </div> : null}
+      <header>
+        <h1 style={{ textAlign: "left", margin: "0 auto" }}>Fresh</h1>
+      </header>
+      <div className="uploadSection">
+        <h2>Add Food</h2>
+        { image !== "" ?
+          <img src={image} alt="food" width="200" height="200"/>
+              :
+          <p></p> 
+        }
+        <input 
+          type="file"
+          className= "text-sm border rounded -lg cursor-pointer"
+          // accept="image/*"
+          onChange={e => handleFileChange(e)}
+          // onChange={(e) => setImage(e.target.files[0])}
+          /><br></br>
+        <button type="submit" className="button" onClick={handleSubmit}>Submit</button>
+        </div>
+          { openAIResponse?.food !== "" ? 
+          <div className="foodLog">
+            <div className="day">
+              <h2>Today</h2>
+              <p><b>Foods:</b> {openAIResponse?.food}</p>
+              <p><b>Calories:</b> {openAIResponse?.calories}</p>
+              <p><b>Protein:</b> {openAIResponse?.protein}</p>
+            </div>
+        </div> : null}
+          <div className="foodLog">
+            <div className="day">
+              <h2>Yesterday</h2>
+              <p><b>Foods:</b> Chicken, Broccoli, Potatoes, Pasta</p>
+              <p><b>Calories:</b> 2,055</p>
+              <p><b>Protein:</b> 117</p>
+            </div>
+        </div>
     </div>
   );
 }
 
-    /* function callAPI() {
+    /*
+
+        <p>{new Date().toLocaleString()}</p>
+        <div className="foodLog">
+        <div className="day">
+          <h2 className="dayTitle">July 31st</h2>
+          <p>Calories: 2,150</p>
+          <p>Protein: 120</p>
+        </div>
+      </div>
+    
+    function callAPI() {
         if (image !== null) {
             console.log(image);
             console.log(PROMPT_TEMPLATE);
