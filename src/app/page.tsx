a85917a48201b862e96cf22d0833b3de8561b8aa
Jacob Trebil <jacobtrebil@yahoo.com>
2024-08-18 13:44:52 -0400

    updated some of the branding to Fresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 import glow from "../../public/glow.png";  
 import dieScore from "../../public/dieScore.png";
 import sadSun from "../../public/sadSun.png";
+import fresh from "../../public/freshleaf.png";
+import veggies from "../../public/grapes.png";
+import leaf from "../../public/leaf22.png";
 import Image from "next/image";
  
 export default function Chat() {
@@ -10,35 +13,89 @@ export default function Chat() {
     window.location.href = '/fooddetection';
   }
 
-  const goToWhoopData = () => {
-    window.location.href = '/whoop';
-  }
-
   const goToHealthGPT = () => {
     window.location.href = '/coach';
+  }
+
+  const goToNutritionTracking = () => {
+    window.location.href = '/tracking';
   }
 
   return (
     <div className="main">
       <div className="logoSection">
-        <h1 className="freshHeadline">Glow</h1>
-        <Image src={glow} alt="glow" className="glow" width={60} />
+        <h1 className="freshHeadline">Fresh</h1>
+        <Image src={leaf} alt="leaf" className="glow" width={60} />
+      </div>
+      <div className="glowBubbleSection">
+        <div className="glowSuggestionBubble">
+          <p className="suggestion">You've been snacking a lot! Reducing your eating frequency to 3 meals / day will reduce the aging damage of food</p>
+        </div>
       </div>
       <div className="mainButtons">
         <button className="mainButton" onClick={goToFoodDetection}>Live Analysis→</button>
         <button className="mainButton" onClick={goToHealthGPT}>Health Coach →</button>
-        <div className="suggestionsSection">
-          <Image src={sadSun} alt="dieScore" className="dieScoreImg" width={150} />
-          <div className="dieScoreSection">
-            <p className="dieScore">Die Score</p>
-            <p className="score">1.15</p>
-          </div>
-          <p className="streak">Streak: 5 days</p>
-          <p className="suggestion">Glow: You've been snacking a lot. Reducing your eating frequency to 3 meals / day will reduce the aging damage of the foods that you eat.</p>
-        </div>
       </div>
     </div>
   );
 }
 
 /*       <a href="https://chat.openai.com/" target="_blank" ><button className="button">Health Coach →</button></a><br></br> */ 
+
+/* 
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+  const data = [
+    {
+      name: 'Monday',
+
+      amt: 0.9,
+    },
+    {
+      name: 'Tuesday',
+      amt: 0.95,
+    },
+    {
+      name: 'Wednesday',
+      amt: 1.1,
+    },
+    {
+      name: 'Thursday',
+      amt: 0.8,
+    },
+    {
+      name: 'Friday',
+      amt: 0.85,
+    },
+    {
+      name: 'Saturday',
+      amt: 1.02,
+    },
+    {
+      name: 'Sunday',
+      amt: 1,
+    },
+  ];
+
+          <button className="mainButton" onClick={goToNutritionTracking}>Nutrition Log →</button>
+          <p className="streak">Streak: 5 days</p>
+          <p className="suggestion">Glow: You've been snacking a lot! Reducing your eating frequency to 3 meals / day will reduce the aging damage of food</p>
+
+          <div className="suggestionsSection">
+            <div className="dieScoreFullSection">
+              <Image src={sadSun} alt="dieScore" className="dieScoreImg" width={150} />
+              <div className="dieScoreSection">
+                <p className="dieScore">Die Score</p>
+                <p className="score">1.15</p>
+              </div>
+            </div>
+          </div>
+
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={300} height={50} data={data}>
+              <Line type="monotone" dataKey="pv" stroke="#FADA07" strokeWidth={2} />
+            </LineChart>
+          </ResponsiveContainer>
+
+*/ 
---
+++ src/app/tracking/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+ 
+import React from 'react';
+ 
+export default function Tracking() {
+
+  return (
+    <div className="nutritionTracking">
+      <div className="nutritionTrackingSection">
+        <h2>Automated Nutrition Tracking</h2>
+      </div>
+    </div>
+  );
+}
